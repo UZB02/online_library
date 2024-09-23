da70474914de35cf4644e415d9a266cc313f0f9a
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-09-23 19:08:41 +0500

    library firs section

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Nav from "./components/Navbar/AppNavigation.vue"
+</script>
+
+<template>
+  <header class="sticky top-0 z-[500]">
+    <Nav></Nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped></style>
