7a997afc7280868f2d09bf34dd4b0cd5c0c2e757
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-09-30 22:22:04 +0500

    Catalog

---
+++ src/components/Catalog/SpeciesCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="flex flex-col gap-2 py-5">
+    <h1 class="w-1/2">Mashhur janrlar</h1>
+   <div class="flex items-center flex-wrap justify-center gap-2 py-5">
+     <div v-for="item in data" class="card  rounded p-4 flex gap-2 cursor-pointer shadow">
+      <div>
+        <img
+          class="w-20 md:w-24"
+          :src="item.cover"
+          alt="Image"
+        />
+      </div>
+      <div class="flex flex-col items-center justify-center gap-3">
+        <h1 class="md:text-xl font-bold">{{item.title}}</h1>
+        <div class="flex flex-col items-center justify-center gap-2">
+          <span class="flex w-full items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              fill="grey"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M23 5v13.883l-1 .117v-16c-3.895.119-7.505.762-10.002 2.316-2.496-1.554-6.102-2.197-9.998-2.316v16l-1-.117v-13.883h-1v15h9.057c1.479 0 1.641 1 2.941 1 1.304 0 1.461-1 2.942-1h9.06v-15h-1zm-12 13.645c-1.946-.772-4.137-1.269-7-1.484v-12.051c2.352.197 4.996.675 7 1.922v11.613zm9-1.484c-2.863.215-5.054.712-7 1.484v-11.613c2.004-1.247 4.648-1.725 7-1.922v12.051z"
+              />
+            </svg>
+            <h3 class="text-sm">{{ item.books }} Kitob</h3>
+          </span>
+          <span class="flex items-center justify-center gap-2">
+            <svg
+              width="24"
+              fill="grey"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path
+                d="M22 13c-.027-5.497-4.497-10-10-10s-9.973 4.503-10 10h2.5c.828 0 1.5.672 1.5 1.5v7c0 .828-.672 1.5-1.5 1.5h-3c-.828 0-1.5-.672-1.5-1.5v-8.5c.027-6.601 5.394-12 12-12s11.973 5.399 12 12v8.5c0 .828-.672 1.5-1.5 1.5h-3c-.828 0-1.5-.672-1.5-1.5v-7c0-.828.672-1.5 1.5-1.5h2.5z"
+              />
+            </svg>
+            <h3 class="text-sm">{{ item.audiobooks }} Audiokitob</h3>
+          </span>
+        </div>
+      </div>
+    </div>
+   </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const data=ref([
+    {
+        id: 1,
+        title: "O'zbek adabiyoti",
+        cover: "https://api.kitob.itsm.uz/data/genres/cover/3.png",
+        books: 610,
+        audiobooks: 500
+    },
+    {
+        id: 2,
+        title: "Jahon Adabiyoti",
+        cover: "https://api.kitob.itsm.uz/data/genres/cover/12.png",
+        books: 780,
+        audiobooks: 650
+    },
+    {
+        id: 3,
+        title: "Tarix",
+        cover: "https://api.kitob.itsm.uz/data/genres/cover/18.png",
+        books: 50,
+        audiobooks: 24
+    },
+    {
+        id: 4,
+        title: "Xorijiy Adabiyotlar",
+        cover: "https://api.kitob.itsm.uz/data/genres/cover/236.png",
+        books: 400,
+        audiobooks: 350
+    }
+])
+</script>
+<style scoped></style>
---
+++ src/views/CatologView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="top w-full bg-slate-200">
+        <div class="container md:h-[30vh] py-8 mx-auto flex items-center justify-center">
+            <span class="w-[80%]">
+                <h1 class="text-2xl md:text-4xl text-green-500 font-bold">Kutibxona</h1>
+                <p class="text-slate-500 text-sm">BMLibrary.uz kutubxonasiga xush kelibsiz</p>
+            </span>
+        </div>
+    </div>
+    <section>
+        <div class="container mx-auto flex flex-col gap-7  justify-center">
+            <SpeciesCards />
+        </div>
+    </section>
+</template>
+<script setup>
+import SpeciesCards from '../components/Catalog/SpeciesCard.vue'
+</script>
+<style scoped>
+
+</style>
